a2137e1fb652ab5e28414dd3e456f259d7ca3476
Michael Hand <animdir@gmail.com>
2018-08-17 18:54:20 -0500

    Initial Commit

---
+++ fun2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  fun2
+//
+//  Created by Michael Hand on 8/17/18.
+//  Copyright © 2018 Michael Hand. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
